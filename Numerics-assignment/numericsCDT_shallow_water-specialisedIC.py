a2f4ce74a30b38f77a9f2e3dc1123cc9bbb75f9c
joey-foster <j.foster@soton.ac.uk>
2024-10-18 18:37:35 +0100

    Implemented semi-local GIF generation

--- Numerics-assignment/numericsCDT_shallow_water-specialisedIC.py
+++ Numerics-assignment/numericsCDT_shallow_water-specialisedIC.py
@@ -1,4 +1,3 @@
-# BRANCH TEST!
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -12,8 +11,6 @@ params = {'text.usetex' : True,
 plt.rcParams.update(params) 
 fig_width_inches = 1 / 72.27 * 443.57848
 fig_height_inches = fig_width_inches * ( -1 + np.sqrt(5) ) / 2
-
-fig, ax = plt.subplots()
 
 #%% Initial data
 def h0(x):
@@ -38,73 +35,84 @@ u = u0(x)
 hOld = h.copy()
 uOld = u.copy()
 
-#H = np.mean(h)
+def GIFtime(hOld,uOld,t=0,current_time=0):
+    fig, ax = plt.subplots()
 
-intialStableWaveSpeed = np.max(u) + np.sqrt(np.max(g*h))
-dt =  0.99 *dx / intialStableWaveSpeed #CFL 
+    intialStableWaveSpeed = np.max(uOld) + np.sqrt(np.max(g*hOld))
+    dt =  0.99 *dx / intialStableWaveSpeed #CFL 
+    
+    nt = int(t_end/dt)
+    
+    #%% Animation/plotting setup
+    
+    fig, ax = plt.subplots(1,2,figsize=(fig_width_inches, fig_height_inches))
+    line_h, = ax[0].plot([], [],'b', label='h')
+    line_u, = ax[1].plot([], [],'r', label='u')
+    
+    ax[0].set_ylabel('$h$')
+    ax[1].set_ylabel('$u$')
+    ax[0].set_ylim([0.9,2])
+    ax[1].set_ylim([7.5,12])
+    for a in ax:
+        a.legend(loc='upper left')
+        a.set_xlabel('$x$')
+        a.set_xlim([-1,1])
+    plt.tight_layout()
+    suptitle = fig.suptitle('Non-linear 1-D SWE with specialised IC to make FTBS work')
+    
+    plt.subplots_adjust(top=0.85)
 
-nt = int(t_end/dt)
-
-#%% Animation/plotting setup
-
-fig, ax = plt.subplots(1,2,figsize=(fig_width_inches, fig_height_inches))
-line_h, = ax[0].plot([], [],'b', label='h')
-line_u, = ax[1].plot([], [],'r', label='u')
-
-ax[0].set_ylabel('$h$')
-ax[1].set_ylabel('$u$')
-ax[0].set_ylim([0.9,2])
-ax[1].set_ylim([7.5,12])
-for a in ax:
-    a.legend(loc='upper left')
-    a.set_xlabel('$x$')
-    a.set_xlim([-1,1])
-plt.tight_layout()
-suptitle = fig.suptitle('Non-linear 1-D SWE with specialised IC to make FTBS work')
-
-plt.subplots_adjust(top=0.85)
-
+    evolution = doEvolution(hOld, uOld, dt, t, current_time, line_h, line_u, suptitle)
+    
+    ani = FuncAnimation(fig, evolution.timestep, frames=nt, blit=False, repeat=False)
+    ani.save('FTBS_NLSWE_specialIC.gif', writer='pillow', fps=20)
+    plt.show()
 #%% Time stepping
 
 ## Using Duran's NL-SWE formualtion
 
 t=0
 current_time = 0
-def timestep(frame):
-#while current_time < t_end:
-    global hOld, uOld, dt, t, current_time
-    #Vectorised FTBS
-    h[1:] = hOld[1:] - dt/dx * (uOld[1:] * (hOld[1:] - hOld[:-1]) + hOld[1:] * (uOld[1:] - uOld[:-1]))
-    u[1:] = uOld[1:] - dt/dx * (uOld[1:] * (uOld[1:] - uOld[:-1]) + g * (hOld[1:] - hOld[:-1]))
 
-    #PBCs
-    h[0] = h[-1] 
-    u[0] = u[-1]
+class doEvolution:
+    
+    def __init__(self, hOld, uOld, dt, t, current_time, line_h, line_u, suptitle):
+        self.hOld = hOld
+        self.uOld = uOld
+        self.dt = dt
+        self.t = t
+        self.current_time = current_time
+        self.line_h = line_h
+        self.line_u = line_u
+        self.suptitle = suptitle
+        
 
-    hOld = h.copy()
-    uOld = u.copy()
+    def timestep(self, frame):
+    #while current_time < t_end: 
+        #Vectorised FTBS
+        h[1:] = self.hOld[1:] - self.dt/dx * (self.uOld[1:] * (self.hOld[1:] - self.hOld[:-1]) + self.hOld[1:] * (self.uOld[1:] - self.uOld[:-1]))
+        u[1:] = self.uOld[1:] - self.dt/dx * (self.uOld[1:] * (self.uOld[1:] - self.uOld[:-1]) + g * (self.hOld[1:] - self.hOld[:-1]))
     
-    uTemp = abs(uOld) + np.sqrt(g*hOld)
-    dt = min(dx / max(uTemp), dt)   
+        #PBCs
+        h[0] = h[-1] 
+        u[0] = u[-1]
+    
+        self.hOld = h.copy()
+        self.uOld = u.copy()
+        
+        uTemp = abs(self.uOld) + np.sqrt(g*self.hOld)
+        self.dt = min(dx / max(uTemp), self.dt)   
+    
+        
+        self.line_h.set_data(x, h)
+        self.line_u.set_data(x, u)
+        
+        self.suptitle.set_text(f'Non-linear 1-D SWE with specialised IC to make FTBS work\n Time = {self.current_time:.3f}')
+        print(f"t={self.t}, dt={self.dt:.5f}, current_time={self.current_time:.3f}, CFL={max(abs(u))*self.dt/dx:.2f}")
+        plt.pause(0.01)
+        
+        self.current_time += self.dt
+        self.t += 1
+  
 
-    
-    line_h.set_data(x, h)
-    line_u.set_data(x, u)
-    
-    suptitle.set_text(f'Non-linear 1-D SWE with specialised IC to make FTBS work\n Time = {current_time:.3f}')
-    print(f"t={t}, dt={dt:.5f}, current_time={current_time:.3f}, CFL={max(abs(u))*dt/dx:.2f}")
-    plt.pause(0.01)
-    
-    current_time += dt
-    t += 1
-    
-    
-    
-    #return t, current_time, dt, hOld, uOld
-#  
-#partial(timestep,hOld=hOld,uOld=uOld,dt=dt,t=t,current_time=current_time
-ani = FuncAnimation(fig, timestep, frames=int(t_end/dt), blit=False, repeat=False)
-ani.save('FTBS_NLSWE_specialIC.gif', writer='pillow', fps=20)
-plt.show()
-    
-#doEvolution(g, nx, h0, u0, t_end)
+GIFtime(hOld, uOld)
