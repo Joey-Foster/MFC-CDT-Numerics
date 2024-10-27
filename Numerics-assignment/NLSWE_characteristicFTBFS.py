64678a5f8f4bffb9f9879a0072a13100c65a6c2f
joey-foster <j.foster@soton.ac.uk>
2024-10-27 16:49:25 +0000

    restored GIFtime() functionality

--- Numerics-assignment/NLSWE_characteristicFTBFS.py
+++ Numerics-assignment/NLSWE_characteristicFTBFS.py
@@ -13,7 +13,7 @@ def h0(x):
 def u0(x):
     return 0*x
 
-def processInitialData(h0, u0, nx, t_end, g, H):
+def processInitialData(h0, u0, nx, g, H, t_end=False):
     
     # Define space discretisation
     dx = 1/nx
@@ -35,29 +35,35 @@ def processInitialData(h0, u0, nx, t_end, g, H):
     stableWaveSpeed = np.sqrt(g*H)
     dt =  0.99 * dx / (2 * stableWaveSpeed) 
      
-    # Ensure number of timesteps is reachable
-    nt = int(np.ceil(t_end/dt))
+    if t_end:
+        # Ensure number of timesteps is reachable
+        nt = int(np.ceil(t_end/dt))
     
-    return Q1Old, Q2Old, Q1, Q2, h, u, x, dx, dt, nt
+        return Q1Old, Q2Old, Q1, Q2, h, u, x, dx, dt, nt
+    else: 
+        return Q1Old, Q2Old, Q1, Q2, h, u, x, dx, dt
 
-def produceStaticPlot(h0, h_lims, u_lims, nx, g, H, t_end, t_simulation_range, t_plotting_range, t_sample, suptitle, filename):
-    if t_sample > 4:
-        raise Exception("t_sample must be between 1 and 4 because I didn't have time to generalise this")
+def produceStaticPlot(h0, h_lims, u_lims, nx, g, H, t_simulation_range, t_plotting_range, t_sample, suptitle, filename):
     current_time=0
 
     fig, ax = plottingSetup(h_lims, u_lims)
     
     fig.suptitle(suptitle)
     
-    Q1Old, Q2Old, Q1, Q2, h, u, x, dx, dt, nt = processInitialData(h0, u0, nx, t_end, g, H)
+    Q1Old, Q2Old, Q1, Q2, h, u, x, dx, dt = processInitialData(h0, u0, nx, g, H)
     
+    # uOld and hOld are carry-overs from the doEvolution parent class but are
+    # not used in this implementation, so they are just defined here as a 
+    # placeholder string. There is definitely a more elegant way to deal with
+    # this, but this works for now. If I have time, I will come back and try to
+    # make this nicer.
     uOld = hOld = 'placeholder'
     
     for t in range(t_simulation_range + 1):
         justPlotTheseTimesteps(t_simulation_range, t_plotting_range, t_sample, t, current_time, ax, x, h, u)
-        evolution = doCharacteristicEvolution(hOld, uOld, h, u, x, dx, nx, dt, nt, 
-                                             t, current_time, t_end, g, H, Q1, Q2, Q1Old, Q2Old)
-        Q1Old, Q2Old, Q1, Q2, h, u, dt, nt, t, current_time = evolution.timestep('')
+        evolution = doCharacteristicEvolution(hOld, uOld, h, u, x, dx, dt, 
+                                             t, current_time, g, H, nx, Q1, Q2, Q1Old, Q2Old)
+        Q1Old, Q2Old, Q1, Q2, h, u, dt, t, current_time = evolution.timestep('')
     fig.legend(loc='upper center', bbox_to_anchor=(0.525, 0.95), ncol=t_sample, frameon=False)
     plt.savefig(f'{filename}.pdf')
     plt.show()
@@ -69,23 +75,17 @@ def GIFtime():
 
     # Set up axes and initial data
     fig, line_h, line_u = plottingSetup([0.9,2], [-1,1], GIF=True)
-    Q1Old, Q2Old, Q1, Q2, h, u, x, dx, dt, nt = processInitialData(h0, u0, nx, 
-                                                                  t_end, g, H)
+    Q1Old, Q2Old, Q1, Q2, h, u, x, dx, dt, nt = processInitialData(h0, u0, nx, g, H, t_end)
    
     suptitle = fig.suptitle('Non-linear 1-D SWE with arbitrary smooth IC')
    
     print('Rendering GIF. Please stand by.')
    
-    # uOld and hOld are carry-overs from the doEvolution parent class but are
-    # not used in this implementation, so they are just defined here as a 
-    # placeholder string. There is definitely a more elegant way to deal with
-    # this, but this works for now. If I have time, I will come back and try to
-    # make this nicer.
+    
     uOld = hOld = 'placeholder'
    
-    evolution = doCharacteristicEvolution(hOld, uOld, h, u, x, dx, nx, dt, nt, 
-                                         t, current_time, Q1, Q2, Q1Old, Q2Old, line_h, line_u, 
-                                         suptitle)
+    evolution = doCharacteristicEvolution(hOld, uOld, h, u, x, dx, dt, 
+                                         t, current_time, g, H, nx, Q1, Q2, Q1Old, Q2Old, nt, t_end, line_h, line_u, suptitle)
     
     ani = FuncAnimation(fig, evolution.timestep, frames=nt, blit=False, 
                        repeat=False)
@@ -96,9 +96,9 @@ def GIFtime():
 
 class doCharacteristicEvolution(doEvolution):
     
-    def __init__(self, hOld, uOld, h, u, x, dx, nx, dt, nt, t, current_time, t_end, g, H, Q1, Q2, Q1Old, Q2Old, line_h=0, line_u=0, suptitle=''):
-        super().__init__(hOld, uOld, h, u, x, dx, dt, nt, t, current_time, t_end, g,
-                     line_h = line_h, line_u = line_u, suptitle = suptitle)
+    def __init__(self, hOld, uOld, h, u, x, dx, dt, t, current_time, g, H, nx, Q1, Q2, Q1Old, Q2Old, nt=False, t_end=False, line_h=False, line_u=False, suptitle=False):
+        super().__init__(hOld, uOld, h, u, x, dx, dt, t, current_time, g, 
+                     nt = nt, t_end = t_end, line_h = line_h, line_u = line_u, suptitle = suptitle)
         self.H = H
         self.nx = nx
         self.Q1 = Q1
@@ -130,29 +130,33 @@ class doCharacteristicEvolution(doEvolution):
         self.u = self.Q1 - self.Q2
         
         # Set axis data
-        if (self.line_h != 0) & (self.line_u != 0):
+        if bool(self.line_h) & bool(self.line_u):
             self.line_h.set_data(self.x, self.h)
             self.line_u.set_data(self.x, self.u)
             
-        if self.suptitle != '':
+        if bool(self.suptitle):
             self.suptitle.set_text(f'Non-linear 1-D SWE with arbitrary smooth IC'
                                f'\n Time = {self.current_time:.3f}')
         print(f't={self.t}, dt={self.dt:.5f}, '
               f'current_time={self.current_time:.3f}, ' 
               f'CFL={2*np.sqrt(self.g*self.H)*self.dt/self.dx:.2f}')
         
-        self.timeOvershootChecker(self.t_end)
-        self.current_time += self.dt
-        self.t += 1
+        if bool(self.t_end) & bool(self.nt):
+            self.timeOvershootChecker(self.t_end)
+            self.current_time += self.dt
+            self.t += 1
+            return self.Q1Old, self.Q2Old, self.Q1, self.Q2, self.h, self.u, self.dt, self.nt, self.t, self.current_time
+        else:
+            self.current_time += self.dt
+            return self.Q1Old, self.Q2Old, self.Q1, self.Q2, self.h, self.u, self.dt, self.t, self.current_time
 
-        return self.Q1Old, self.Q2Old, self.Q1, self.Q2, self.h, self.u, self.dt, self.nt, self.t, self.current_time
 #%% Params
 
 if __name__ == '__main__':
     g = 9.81
     H = 1
     nx = 100
-    t_end = 1
+    t_end = 0.1
 
-    #GIFtime()
-    produceStaticPlot(h0, h_lims = [0.9,2], u_lims = [-1,1], nx = nx, g = g, H = H, t_end = t_end, t_simulation_range = 200, t_plotting_range = [180,200], t_sample = 4, suptitle = r'Non-linear 1-D SWE with $h_0 = 1 + e^{-5x^2}$, $u_0 = 0$', filename = 'NLSWE_arbitraryIC')
+    GIFtime()
+    #produceStaticPlot(h0, h_lims = [0.9,2], u_lims = [-1,1], nx = nx, g = g, H = H, t_end = t_end, t_simulation_range = 200, t_plotting_range = [180,200], t_sample = 4, suptitle = r'Non-linear 1-D SWE with $h_0 = 1 + e^{-5x^2}$, $u_0 = 0$', filename = 'NLSWE_arbitraryIC')
