806729820062c4405d743a16bacddbaf7ddaae42
joey-foster <j.foster@soton.ac.uk>
2024-11-19 10:19:12 +0000

    some of this works, but not all...

--- Numerics_part_2_chpt3exercises.py
+++ Numerics_part_2_chpt3exercises.py
@@ -24,46 +24,65 @@ x_numeric = np.linspace(0, 1, Ne+1)
 plt.plot(x_analytic, psi_analytic1(x_analytic),'-b',label='analytic')
 plt.plot(x_numeric, psi, '--r', label='numeric')
 plt.grid()
+plt.xlabel(r'$x$')
+plt.ylabel(r'$\psi$')
 plt.legend()
-plt.title('Exercise 3.1(c) solution: Ne=2 solved globally over nodes')
+plt.title('Exercise 3.1(c) solution: Solve globally over nodes\n' r'Ne = 2 and $S(x) = 1-x$')
 plt.show()
 
 #%% Exercise 3.2
-plt.figure()
 
-def OneDimFESolver(Ne, S, DirichletL=False, DirichletR=False, NeumannL=False, NeumannR=False):
+def OneDimFESolver(Ne, S, DirichletL=None, DirichletR=None, NeumannL=None, NeumannR=None):
+    """
+    Solves the one-dimensional PDE problem \partial_xx \psi = -S(x) using the 
+    finite element method on an unstructured grid
+
+    Note: Providing exactly two of the 4 possible boundary conditions is necessary 
+    for a well-posed PDE problem. Double-Neumann is ill-posed.
+
+    Parameters:
+    Ne (int): Number of elements.
+    S (function): Source function S(x).
+    DirichletL (float, optional): Dirichlet boundary condition at the left end. Default is None.
+    DirichletR (float, optional): Dirichlet boundary condition at the right end. Default is None.
+    NeumannL (float, optional): Neumann boundary condition at the left end. Default is None.
+    NeumannR (float, optional): Neumann boundary condition at the right end. Default is None.
+
+    Returns:
+    tuple: A tuple containing the nodes and the solution vector.
+    """
     
     nodes, dx = np.linspace(0, 1, Ne+1, retstep=True)
     #nodes = np.array([0, *sorted(np.random.rand(Ne-1)), 1])
-    print(len(nodes))
     
     DirichletCount = 0
-    if type(DirichletL)!=bool:
+    if DirichletL != None:
         DirichletCount += 1
-    if type(DirichletR)!=bool:
+    if DirichletR != None:
         DirichletCount += 1
         
-    MaxSolvingDimension = Ne+1-DirichletCount
+    MaxSolvingDimension = Ne + 1 - DirichletCount
     
-    LM = np.zeros((2, MaxSolvingDimension), dtype=np.int64)
-    for e in range(MaxSolvingDimension):
+    
+    LM = np.zeros((2, Ne), dtype=np.int64)
+    for e in range(Ne):
         if e==0:
-            if type(DirichletL)!=bool:
+            if DirichletL != None:
                 LM[0, e] = -1 # Don't consider the left BP
                 LM[1, e] = 0
             else:
                 LM[0, e] = 0
                 LM[1, e] = 1
-        elif 0 < e < MaxSolvingDimension - 1:
+        elif 0 < e < Ne - 1:
             LM[0, e] = LM[1, e-1]
             LM[1, e] = LM[0, e] + 1
         else: 
             LM[0, e] = LM[1, e-1]
-            if type(DirichletR)!=bool:
+            if DirichletR != None:
                 LM[1, e] = -1 # Don't consider right BP
             else:
-                LM[1, e] = LM[0, e]
-            
+                LM[1, e] = LM[0, e] + 1
+    
     print(LM)
     
     def stiffness(element):
@@ -81,10 +100,11 @@ def OneDimFESolver(Ne, S, DirichletL=False, DirichletR=False, NeumannL=False, Ne
         force_vector[1] = dx/2 * scipy.integrate.quad(integrand2, -1,1)[0]
         return force_vector
             
+    print(MaxSolvingDimension)
     
     K = np.zeros((MaxSolvingDimension, MaxSolvingDimension))
     F = np.zeros((MaxSolvingDimension,))
-    for e in range(MaxSolvingDimension):
+    for e in range(Ne):
         k_e = stiffness(nodes[e:e+2])
         f_e = force(nodes[e:e+2], S)
         for a in range(2):
@@ -96,32 +116,32 @@ def OneDimFESolver(Ne, S, DirichletL=False, DirichletR=False, NeumannL=False, Ne
             if (A >= 0):
                 F[A] += f_e[a]
         
-  
         # Modify force vector for Dirichlet BC
-        if e == 0 and type(DirichletL)!=bool:
+        if e == 0 and DirichletL != None:
             F[0] -= DirichletL * k_e[1, 0]
-        if e == MaxSolvingDimension - 1 and type(DirichletR)!=bool:
+        if e == MaxSolvingDimension - 1 and DirichletR != None:
             F[-1] -= DirichletR * k_e[1, 0]
-    print(f'K = {K}')
-
+            
     # Modify force vector for Neumann BC
-    if type(NeumannL)!=bool:
+    if NeumannL != None:
         F[0] += NeumannL
-    if type(NeumannR)!=bool:
+    if NeumannR != None:
         F[-1] += NeumannR
-   
+    print(F)
     
     Psi_A = np.zeros_like(nodes)
     Lindex = 0
-    Rindex = len(Psi_A)+1
-    if type(DirichletL)!=bool:
+    Rindex = len(Psi_A) 
+    
+    if DirichletL != None:
         Psi_A[0] = DirichletL
         Lindex = 1
-    if type(DirichletR)!=bool:
+    if DirichletR != None:
         Psi_A[-1] = DirichletR
         Rindex = -1
-    Psi_A[Lindex:Rindex] = np.linalg.solve(K, F)
 
+    Psi_A[Lindex:Rindex] = np.linalg.solve(K, F)
+    
     print(Psi_A)
 
     return nodes, Psi_A
@@ -154,13 +174,52 @@ def psi_analytic3(x):
             output[i] = -0.2*x_i + 0.35
     return output
 
-nodes, soln = OneDimFESolver(7, S1, DirichletL=0, DirichletR=1)
+###############################################################################
+
+Ne=7
+
+plt.figure()
+
+nodes, soln = OneDimFESolver(Ne, S2, NeumannL=1/2, DirichletR=1/12)
+#nodes, soln = OneDimFESolver(Ne, S2, DirichletL=0, NeumannR=0)
+#nodes, soln = OneDimFESolver(Ne, S2, DirichletL=0, DirichletR=1/12)
 
 x_analytic = np.linspace(0, 1, 100)
 
-#plt.plot(x_analytic, psi_analytic3(x_analytic),'-b',label='analytic')
+plt.plot(x_analytic, psi_analytic2(x_analytic),'-b',label='analytic')
 plt.plot(nodes, soln, '--x', color='r', label='numeric')
 plt.grid()
+plt.xlabel(r'$x$')
+plt.ylabel(r'$\psi$')
 plt.legend()
-plt.title(f'Exercise 3.2 solution: Ne={Ne} solved locally over elements')
+plt.title(f'Exercise 3.2 solution: Solve locally over elements\n Ne = {Ne}')
 plt.show()
+
+
+
+
+# sources = [S1, S2, S3]
+# analytics = [psi_analytic1, psi_analytic2, psi_analytic3]
+# DirichletL = [0, None, 0.1]
+# DirichletR = [1/6, 1/12, None]
+# NeumannL = [None, 1/3, None]
+# NeumannR = [None,None,-0.2]
+# latexsourcetitles = ['$S(x) = 1-x$', '$S(x) = (1-x)^2$','$S(x) = \{1$ in the centre, piecewise}']
+
+# for i in range(3):
+#     plt.figure()
+
+#     nodes, soln = OneDimFESolver(Ne, sources[i], 
+#                                  DirichletL=DirichletL[i], DirichletR=DirichletR[i],
+#                                  NeumannL=NeumannL[i], NeumannR=NeumannR[i])
+    
+#     x_analytic = np.linspace(0, 1, 100)
+    
+#     plt.plot(x_analytic, analytics[i](x_analytic),'-b',label='analytic')
+#     plt.plot(nodes, soln, '--x', color='r', label='numeric')
+#     plt.grid()
+#     plt.xlabel(r'$x$')
+#     plt.ylabel(r'$\psi$')
+#     plt.legend()
+#     plt.title(f'Exercise 3.2 solution: Solve locally over elements\n Ne = {Ne}, {latexsourcetitles[i]}')
+#     plt.show()
