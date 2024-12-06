86bd101290795ea97a6df2f345bc2a9272039cea
joey-foster <j.foster@soton.ac.uk>
2024-12-06 14:32:46 +0000

    added more convergence stuff that I'm not convinced works

--- Numerics-assignment-2/staticPollutionOverReading.py
+++ Numerics-assignment-2/staticPollutionOverReading.py
@@ -20,6 +20,26 @@ def nearestElement2Coords(nodes, IEN, coords):
     '''
     nodes should be 2xN
     coords should be 2x1
+    
+    Argument for the validity of this construction: 
+        
+    I am constructing a large circle centred at 'coords' and computing the number 
+    of nodes within the circle, then shrinking the radius until there are exactly 
+    3 nodes within it.
+    In most cases, these 3 nodes are precisely the nodes of the triangle that 
+    contains 'coords'. However, it can be shown geometrically that this is not 
+    always the case. Nevertheless, the local coordinate construction is valid 
+    outside the confines of the reference triangle as it maps all of mathbb{R}^2
+    in principle. (We only care about xi_1,2 inside the reference triangle for 
+    the purposes of solving, but they exist everywhere anyway). Therefore, 
+    relative to a "close enough" triangle, the basis-function construction seen
+    in pollutionExtractor() still holds.
+    The purpose of elementValidityChecker() is to see if the identified triangle
+    (of which the searching radius now defines its circumcircle) is actually
+    one of the triangles in IEN. If so, then it relabels the order of the nodes 
+    found by the search to match the order they appear in the IEN, for consistency
+    reasons. Since for at least one resolution, this test fails (yet I proceed
+    anyway), this check is essentially superfluous but remains for sanity reasons.
     '''
     def locateTriangle(coords):
         error = np.zeros_like(nodes[0,:])
@@ -61,69 +81,76 @@ def pollutionExtractor(psi, nodes, IEN, coords):
     pollution = np.dot(psi[elementnodes], N)
     return pollution
 
-def convergence1(coords, u0, D):
-    '''
-    With current implementation:
-    trendline with all points goes like x^1.7
-    killing the last point gives 2.47
-    killing the last 2 points gives 3.11
-    '''
-    maxres_nodes, maxres_IEN, maxres_southern_boarder, maxres_psi = TwoDimStaticAdvDiffFESolver(S_sotonfire, 10, 10000, '1_25')
+def convergence(coords, u, D):
+   
+    maxres_nodes, maxres_IEN, maxres_southern_boarder, maxres_psi = TwoDimStaticAdvDiffFESolver(S_sotonfire, u, D, '1_25')
     maxres_pollution = pollutionExtractor(maxres_psi, maxres_nodes, maxres_IEN, coords)
+    y_4N = maxres_pollution
     print('computed max res')
    
     error = np.zeros(5)
+    Ns = np.zeros(5)
     for i, res in enumerate(['2_5','5','10','20','40']):
-        nodes, IEN, southern_boarder, psi = TwoDimStaticAdvDiffFESolver(S_sotonfire, u0, D, res)
+        nodes, IEN, southern_boarder, psi = TwoDimStaticAdvDiffFESolver(S_sotonfire, u, D, res)
+        Ns[i] = len(nodes[0,:])**(1/2)
         pollution = pollutionExtractor(psi, nodes, IEN, coords)
+        if res == '5':
+            y_N = pollution
+        elif res == '2_5':
+            y_2N = pollution
         error[i] = abs(pollution - maxres_pollution)
         print(f'computed error for {res}')
-        
-    x = np.array([2500, 5000, 10000, 20000, 40000])
     
-    polyfit_coeffs = np.polyfit(np.log(x[:-1]),np.log(error[:-1]),1) 
+    polyfit_coeffs = np.polyfit(np.log(Ns[:]),np.log(error[:]),1) 
 
     trendline = lambda data,x: np.poly1d(data)(x)
     
-    plt.loglog(x, error, 'xk')
-    plt.loglog(x, np.exp(trendline(polyfit_coeffs,np.log(x))),'-r', label=rf'$\propto x^{{{polyfit_coeffs[0]:.2f}}}$')
-    plt.xlabel('average element side length')
+    plt.figure()
+    plt.loglog(Ns, error, 'xk')
+    plt.loglog(Ns, np.exp(trendline(polyfit_coeffs,np.log(Ns))),'-r', 
+               label=rf'$\propto N^{{{polyfit_coeffs[0]:.2f}}}$')
+    plt.xlabel('sqrt(Number of nodes)')
     plt.ylabel('Error relative to max res soln')
     plt.legend()
     plt.grid()
-    plt.title(f'Relative Error in static case with u = {u0}, D = {D}')
+    plt.title(f'Relative Error in static case with u = [{u[0]:.2f}, {u[1]:.2f}], D = {D}')
+    
+    x = np.array([2500, 5000, 10000, 20000, 40000])
+    polyfit_coeffs = np.polyfit(np.log(x[:-1]),np.log(error[:-1]),1) 
+    
+    plt.figure()
+    plt.loglog(x, error, 'xk')
+    plt.loglog(x, np.exp(trendline(polyfit_coeffs,np.log(x))),'-r', 
+               label=rf'$\propto x^{{{polyfit_coeffs[0]:.2f}}}$')
+    plt.xlabel('Canonical triangle side length (m)')
+    plt.ylabel('Error relative to max res soln')
+    plt.legend()
+    plt.grid()
+    plt.title(f'Relative Error in static case with u = [{u[0]:.2f}, {u[1]:.2f}], D = {D}')
     
     plt.show()
     
-def convergence2(coords, u0, D):
-    
-    soln_N  = TwoDimStaticAdvDiffFESolver(S_sotonfire, u0, D, '5')
-    print('computed 5k')
-    soln_2N = TwoDimStaticAdvDiffFESolver(S_sotonfire, u0, D, '2_5')
-    print('computed 2.5k')
-    soln_4N = TwoDimStaticAdvDiffFESolver(S_sotonfire, u0, D, '1_25')
-    print('computed 1.25k')
-    
-    y_N = pollutionExtractor(soln_N[3], soln_N[0], soln_N[1], coords)
-    y_2N = pollutionExtractor(soln_2N[3], soln_2N[0], soln_2N[1], coords)
-    y_4N = pollutionExtractor(soln_4N[3], soln_4N[0], soln_4N[1], coords)
+    ###########################################################################
     
     y2N_N = abs(y_2N - y_N)
     y4N_2N = abs(y_4N - y_2N)
     
     s = np.log2(y2N_N/y4N_2N)
     
-    error = y2N_N/(1-2**(-s))
+    abs_error = y2N_N/(1-2**(-s))
+    rel_error = abs_error/y_N * 100
 
-    return s, error
+    return s, abs_error, rel_error
+    
     
 #%%
 if __name__ == '__main__':
-
-    nodes, IEN, southern_boarder, psi = TwoDimStaticAdvDiffFESolver(S_sotonfire, 10, 10000, '5')
     
-    #normalising
-    psi = 1/max(psi)*psi
+    directed_at_reading = np.array([473993 - 442365, 171625 - 115483])
+    directed_at_reading = 1/np.linalg.norm(directed_at_reading)*directed_at_reading
+    
+    nodes, IEN, southern_boarder, psi = TwoDimStaticAdvDiffFESolver(S_sotonfire, 
+                                                                    10*directed_at_reading, 10000, '5')
     
     plt.tripcolor(nodes[0,:], nodes[1,:], psi, triangles=IEN)
     plt.plot([442365],[115483],'x',c='r')
@@ -139,6 +166,6 @@ if __name__ == '__main__':
     ans = pollutionExtractor(psi, nodes, IEN, reading)   
     print(ans)
     
-    #convergence1(reading, 10, 10000)
-    order, error = convergence2(reading, 10, 10000)
-    print(f'theoretical convergence order = {order}\n theoretical error = {error}')
+    order, error1, error2 = convergence(reading, 10*np.array([0,1]), 10000)
+    print(f'theoretical convergence order = {order}\n'f'theoretical absolute error = {error1}\n'
+          f'theoretical relative error = {error2}%')
